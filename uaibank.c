a8f9fd107de48b73410cc90c9e05ef7fc5439a16
leolace <lgonsalez02@gmail.com>
2024-05-02 19:48:38 -0300

    minimal fixes and add multiples users

--- uaibank.c
+++ uaibank.c
@@ -3,10 +3,10 @@
 #include <string.h>
 
 typedef struct {
-	int id;
-	char name[101];
-	int age;
-	float current_balance;
+  int   id;
+  char  name[101];
+  int   age;
+  float current_balance;
 } User;
 
 User new_user(int users_qnty) {
@@ -21,7 +21,7 @@ User new_user(int users_qnty) {
   printf("\nDigite a idade do usuário: \n");
   scanf(" %d", &age);
   
-  printf("\nDigite o saldo atual do usuário: \n");
+  printf("\nDigite o saldo atual do usuário: \nR$ ");
   scanf(" %f", &current_balance);
 
   user.id = users_qnty;
@@ -29,37 +29,50 @@ User new_user(int users_qnty) {
   user.age = age;
   user.current_balance = current_balance;
 
+  printf("\nUsuário 0%d adicionado com sucesso\n\n", user.id);
+
   return user;
 }
 
 void main() {
-  User *users = (User*)malloc(sizeof(User));
+  User users[100];
   int users_qnty = 0;
   int opt;
+  printf("Selecione uma opção: \n[1] - Criar um novo usuário\n[2] - Criar multiplos novos usuários\n[3] - Buscar um usuário por ID\n[4] - Deletar um usuário por ID\n[5] - Transferência de saldo entre usuários\n[qualquer outra tecla] - Sair\n-> ");
+  scanf(" %d", &opt);    
 
-  while (opt <= 5 && opt >= 0) {
-    printf("Selecione uma opção: \n[1] - Criar um novo usuário\n[2] - Criar multiplos novos usuários\n[3] - Buscar um usuário por ID\n[4] - Deletar um usu[ario por ID\n[5] - Transferência de saldo entre usuários\n[qualquer outra tecla] - Sair\n-> ");
-    scanf(" %d", &opt);
-    
+  while (opt <= 5 && opt > 0) {
     switch (opt) {
-	case 1:
-	  users[users_qnty] = new_user(users_qnty);
-	  users_qnty++;
-	  break;
-	default:
-	  for (int j = 0; j < users_qnty; j++) {
-	    printf("\n");
-	    printf("ID: %d\nNome: %s\nIdade: %d\nSaldo atual: %f\n", users[j].id, users[j].name, users[j].age, users[j].current_balance);  
-	  };
+    case 1:
+      users[users_qnty] = new_user(users_qnty);
+      users_qnty++;
+ 
+      break;
+    case 2:
+      int qnty;
 
-	  break;
+      printf("Digite a quantidade de usuários: \n->");
+      scanf("%d", &qnty);
+
+      for (int i = qnty; i > 0; i--){
+	users[users_qnty] = new_user(users_qnty);
+	users_qnty++;
       }
+
+      printf("Operação concluida. %d usuários adicionados\n\n", qnty);      
+      
+    default:
+      break;
+    }
+
+    printf("Selecione uma opção: \n[1] - Criar um novo usuário\n[2] - Criar multiplos novos usuários\n[3] - Buscar um usuário por ID\n[4] - Deletar um usuário por ID\n[5] - Transferência de saldo entre usuários\n[qualquer outra tecla] - Sair\n-> ");
+    scanf(" %d", &opt);
   }
 
-  for (int i = 0; i < users_qnty; i++) {
-    users[i] = new_user(i);
+  for (int j = 0; j < users_qnty; j++) {
+    printf("\n");
+    printf("ID: %d\nNome: %s\nIdade: %d\nSaldo atual: %f\n\n", users[j].id, users[j].name, users[j].age, users[j].current_balance);  
   };
-
     
   return;
 }
