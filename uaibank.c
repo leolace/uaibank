2766f3e6cf1d911b7c9822e7cbba97321e42c6b1
leolace <lgonsalez02@gmail.com>
2024-05-19 20:32:49 -0300

    improve functions

--- uaibank.c
+++ uaibank.c
@@ -2,6 +2,12 @@
 #include <stdlib.h>
 #include <string.h>
 
+#ifdef _WIN32
+#include <Windows.h>
+#else
+#include <unistd.h>
+#endif
+
 typedef struct {
   int   id;
   char  name[101];
@@ -34,7 +40,7 @@ User* new_user(int users_qnty) {
   user->age = age;
   user->current_balance = current_balance;
 
-  printf("\nUsuário %d adicionado com sucesso\n\n", user->id);
+  printf("\nUsuário %d adicionado com sucesso\n", user->id);
 
   return user;
 }
@@ -42,46 +48,39 @@ User* new_user(int users_qnty) {
 int search_id(int id, User users[], int users_qnty) {
   for (int i = 0; i < users_qnty; i++) {
     if (users[i].id == id) {
-      printf("ID: %d\nNome: %s\nIdade: %d\nSaldo atual: %.2f\n", users[i].id, users[i].name, users[i].age, users[i].current_balance);
       return i;
     }
   }
 
-
-  printf("\nErro: Usuário %d não encontrado.\n", id);
   return -1;
-
 }
 
 void transfer_money(int id_origin, int id_destination, User users[], int users_qnty, float amount) {
-  for (int i = 0; i < users_qnty; i++) {
-    if (users[i].id == id_origin) {
-      users[i].current_balance -= amount;
-    }
-    if (users[i].id == id_destination) {
-      users[i].current_balance += amount;
-    }
-  }
+  if (users[search_id(id_origin, users, users_qnty)].current_balance - amount < 0) {
+    printf("Saldo insuficiente.\n");
+    return;
+  };
+  
+  users[search_id(id_origin, users, users_qnty)].current_balance -= amount;
+  users[search_id(id_destination, users, users_qnty)].current_balance += amount;
 
   printf("Transferência entre usuários feita com sucesso!\n");
+  return;
 }
 
-void remove_user(int id, User users[], int *users_qnty) { 
-  int found = 0;
-  for (int i = 0; i < *users_qnty; i++) {
-    if (users[i].id == id) {
-      found = 1;
-      for (int j = i; j < *users_qnty - 1; j++) {
-	users[j] = users[j + 1];
-      }
-      (*users_qnty)--;
-      printf("Usuário  %d removido com sucesso.\n", id);
-      break;
-    }
-  }
-  if (!found) {
+void remove_user(int id, User users[]) {
+  int user_index = search_id(id, users, *users_qnty);
+
+  if (user_index == -1) {
     printf("Erro: Usuário  %d não encontrado.\n", id);
+    return;
   }
+  
+  for (int j = user_index; j < *users_qnty - 1; j++) {
+    users[j] = users[j + 1];
+  }
+  
+  printf("Usuário  %d removido com sucesso.\n", id);
 }
 
 const int MAX_USERS_SIZE = 20;
@@ -97,7 +96,7 @@ int main() {
   }
   
   do {
-    printf("Selecione uma opção: \n[1] - Criar um novo usuário\n[2] - Criar multiplos novos usuários\n[3] - Buscar um usuário por ID\n[4] - Deletar um usuário por ID\n[5] - Transferência de saldo entre usuários\n[qualquer outra tecla] - Sair\n-> ");
+    printf("\nSelecione uma opção: \n[1] - Criar um novo usuário\n[2] - Criar multiplos novos usuários\n[3] - Buscar um usuário por ID\n[4] - Transferência de saldo entre usuários\n[5] - Deletar um usuário por ID\n[qualquer outra tecla] - Sair\n-> ");
     scanf(" %d", &opt);    
 
     switch (opt) {
@@ -107,7 +106,7 @@ int main() {
       if(user == NULL) {
 	free(users);
 	printf("Error: memory allocation for user failed");
-	return 1;
+	break;
       }
       
       memcpy(&users[users_qnty], user, sizeof(User));
@@ -118,7 +117,7 @@ int main() {
     case 2: {
       int users_qnty_in_row = 0;
 
-      printf("Digite a quantidade de usuários: \n->");
+      printf("Digite a quantidade de usuários: \n-> ");
       scanf("%d", &users_qnty_in_row);
 
       for (int i = users_qnty_in_row; i > 0; i--) {
@@ -128,7 +127,7 @@ int main() {
 	if(user == NULL) {
 	  free(users);
 	  printf("Error: memory allocation for user failed");
-	  return 1;
+	  break;
 	}
       
 	memcpy(&users[users_qnty], user, sizeof(User));
@@ -137,44 +136,69 @@ int main() {
 	users_qnty++;
       }
 
-      printf("Operação concluída. %d usuários adicionados\n\n", users_qnty_in_row);
+      printf("Operação concluída. %d usuários adicionados\n", users_qnty_in_row);
       break;
     }
     case 3: {
-      int ind; // id do usuário
-      printf("Digite o id do usuário que deseja buscar: ");
-      scanf("%d", &ind);
-      search_id(ind, users, users_qnty);
+      int id;
+      
+      printf("Digite o id do usuário que deseja buscar:\n-> ");
+      scanf("%d", &id);
+      
+      int user_id = search_id(id, users, users_qnty);
+
+      if (user_id == -1) {
+	printf("Erro: Usuário %d não encontrado.\n", id);
+	break;
+      }
+      
+      User user_found = users[user_id];
+      
+      printf("\nID: %d\nNome: %s\nIdade: %d\nSaldo atual: R$ %.2f\n", user_found.id, user_found.name, user_found.age, user_found.current_balance);
       break;
     }
     case 4: {
       int id_origin, id_destination;
-      float amount;
-      printf("Digite o id de origem, id de destino e a quantia que deseja transferir:\n");
-      scanf("%d %d %f", &id_origin, &id_destination, &amount);
-      transfer_money(id_origin, id_destination,users,users_qnty, amount);
+      float amount = 0.0;
+      
+      printf("Digite o ID de origem\n-> ");
+      scanf("%d", &id_origin);
+      
+      printf("Digite o ID de destino\n-> ");
+      scanf("%d", &id_destination);
+      
+      printf("Digite a quantia que deseja transferir\n-> R$ ");
+      scanf("%f", &amount);
+
+      if (!id_origin || !id_destination) {
+	printf("Digite um ID valido.\n");
+        break;
+      }
+      
+      transfer_money(id_origin, id_destination, users, users_qnty, amount);
+      
       break;
-      //ver duvida sobre printar ou n
     }
     case 5:{
-      int id_remove;
-      printf("Digite o ID do usuário que deseja remover: ");
-      scanf("%d", &id_remove);
-      remove_user(id_remove, users, &users_qnty);
+      int id;
+      printf("Digite o ID do usuário que deseja remover:\n-> ");
+      scanf("%d", &id);
+      remove_user(id, users);
       break;
-
     }
 
     default:
       break;
     }
 
+    sleep(1);
+
   } while (opt <= 5 && opt > 0);
 
   for (int j = 0; j < users_qnty; j++) {
     if (users[j].id != 0) {
       printf("\n");
-      printf("ID: %d\nNome: %s\nIdade: %d\nSaldo atual: %.2f\n\n", users[j].id, users[j].name, users[j].age, users[j].current_balance);  
+      printf("ID: %d\nNome: %s\nIdade: %d\nSaldo atual: %.2f\n", users[j].id, users[j].name, users[j].age, users[j].current_balance);  
     }
   };
 
